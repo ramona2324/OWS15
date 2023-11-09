a5f20d0be2329c8832eae5af6298889496469601
raffdevstack <raffy77sm@gmail.com>
2023-11-10 04:34:07 +0800

    added init student routes, installed tailwind

--- routes/student.php
+++ routes/student.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-
-Route::get('/student', [StudentController::class, 'showIndex'])
-    ->name('student_home');
+    // all student routes here
+Route::group(['prefix' => 'student'], function () {
+    Route::get('/', [StudentController::class, 'showIndex'])
+        ->name('student_home');
+    Route::get('/login', [StudentController::class, 'showLogin'])
+        ->name('student_showlogin');
+    Route::get('/signup-step1', [StudentController::class, 'showSignup1'])
+        ->name('signup1');
+    // for google single sign on
+    Route::get('/auth/google/callback/', [GoogleAuthController::class, 'callback'])
+        ->name('google_callback');
+});
