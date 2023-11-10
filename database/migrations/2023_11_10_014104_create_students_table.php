094c49bdc5894f0b65790fc223e71bf97f6d9323
raffdevstack <raffy77sm@gmail.com>
2023-11-10 10:09:05 +0800

    fixed function of google sso

--- database/migrations/2023_11_10_014104_create_students_table.php
+++ database/migrations/2023_11_10_014104_create_students_table.php
@@ -12,7 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->id('student_osasid');
+            $table->string('student_lname', 50); // Limit to 50 characters
+            $table->string('student_fname', 50); // Limit to 50 characters
+            $table->string('email', 100)->unique(); // Limit to 100 characters and make it unique
+            $table->string('student_picture', 255);
+
+            $table->string('student_address', 255)->nullable(); // Limit to 255 characters
+            $table->string('student_course', 100)->nullable(); // Limit to 100 characters
+            $table->string('student_section', 10)->nullable(); // Limit to 10 characters
+
+            $table->string('password')->nullable(); // You should hash the password before storing it
+            $table->Decimal('google_id', 21, 0)->nullable();
             $table->timestamps();
         });
     }
