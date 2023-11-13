e49d02ebac676f1108410f82368ad0b055791d46
raffdevstack <raffy77sm@gmail.com>
2023-11-13 11:57:50 +0800

    done sidebar toggle

--- resources/views/partials/__footer.blade.php
+++ resources/views/partials/__footer.blade.php
@@ -1,2 +1,16 @@
 </body>
+
+{{-- for sidebar button toggle --}}
+<script> 
+    document.addEventListener("DOMContentLoaded", function() {
+        const toggleButton = document.getElementById("sidebtn");
+        const sidebar = document.getElementById("logo-sidebar");
+
+        toggleButton.addEventListener("click", function() {
+            // Toggle the 'hidden' class to show/hide the sidebar
+            sidebar.classList.toggle("translate-x-0");
+        });
+    });
+</script>
+
 </html>
