1e9fba17267e913f322a0d6c48ed0e71c5c03fe9
raffdevstack <raffy77sm@gmail.com>
2023-11-14 10:18:40 +0800

    done initial setup - student dashboard

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,1 +1,38 @@
-student homepage
+@include('partials.__header')
+
+@include('partials.__student_sidebar')
+
+{{-- right side section --}}
+<div class="md:ml-60">
+
+    {{-- reusable page header --}}
+    @include('partials.__student_pageheader')
+   
+    {{-- main content --}}
+    <div class="p-4 m-4 shadow-lg bg-white border-gray-200 rounded-lg " style="min-height: 90vh">
+        {{-- navigation container --}}
+        <div class="flex items-center  mb-4 rounded ">
+            {{-- breadcrumb nav container --}}
+            <nav class="flex" aria-label="Breadcrumb">
+                <ol class="inline-flex items space-x-1 md:space-x-3">
+                    <li aria-current="page" class="inline-flex items-center">
+                        <a href="#"
+                            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 ">
+                            <span class="px-1 material-symbols-rounded" style="font-size:20px">dashboard</span>
+                            Dashboard
+                        </a>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+
+        {{-- 1st, 3 col --}}
+        <div class="grid grid-cols-3 gap-4 mb-4">
+           
+        </div>
+
+    </div>
+</div>
+</div>
+
+@include('partials.__footer')
