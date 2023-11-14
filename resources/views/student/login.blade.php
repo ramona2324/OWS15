131d78afdd243216e931c6edadc7e36934ad0f4a
raffdevstack <raffy77sm@gmail.com>
2023-11-14 09:22:57 +0800

    done putting new images-welcom view

--- resources/views/student/login.blade.php
+++ resources/views/student/login.blade.php
@@ -63,4 +63,6 @@
         </div>
     </div>
 </div>
+
+
 @include('partials.__footer')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,48 @@
 <!-- testing github -->
 <!-- test 2 -->
 
-<div class="flex flex-col justify-center items-center ">
-    <img src="{{ asset('images/osaslogo.png') }}" class="h-10 mt-20 sm:h-40" />
-    <p class="text-dark text-5xl font-medium mt-4">OSAS</p>
-    <p class="text-dark text-3xl font-medium">Web Services</p>
-    <a href="{{ route('student_showlogin') }}">
-        <button type="button"
-            class="mt-20 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-            GET STARTED
-        </button>
-    </a>
+<div class="min-h-screen min-w-full flex flex-col md:flex-row  ">
+    <!--left-->
+    <div class="bg-white ouryellowbg shadow-lg rounded-lg md:w-1/2 sm:w-full min-h-full m-5 md:mr-0  ">
+        <div class="m-3 p-3 md:p-8 rounded-lg flex flex-col relative"style=" height:96%; ">
+            <div class="mb-6">
+                <h1 class="text-3xl font-bold text-white">Welcome OSAS Web Services</h1>
+                <p class="mt-4 text-white text-xl">To get started, please provide the following information to create
+                    your student account.</p>
+            </div>
+            <div class="hidden md:block absolute inset-0 flex items-center justify-center">
+                <!-- Image at the very center -->
+                <img src="/images/student/welcome_vector.png" alt="" class="mt-12 mx-auto w-80 ">
+                {{-- public\images\student\login_image.png --}}
+            </div>
+            <div class="flex justify-center mt-auto">
+                <div class="w-12 h-12 rounded-full ouryellowbg flex items-center justify-center">
+                    <i class='bx bxl-facebook-square' style='color:#ffffff; font-size: 30px;'></i>
+                </div>
+                <div class="w-12 h-12 rounded-full ouryellowbg flex items-center justify-center ml-4">
+                    <i class='bx bx-world' style="color:#ffffff; font-size: 30px;"></i>
+                </div>
+                <div class="w-12 h-12 rounded-full ouryellowbg flex items-center justify-center ml-4">
+                    <i class='bx bxl-gmail' style='color:#ffffff; font-size: 30px;'></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--right-->
+    <div
+        class="shadow-lg p-8 bg-white rounded-lg min-h-full m-5 mt-0 md:w-1/2 min-h-full flex flex-col sm:w-full md:mt-5">
+        <div class="flex flex-col justify-center items-center ">
+            <img src="{{ asset('images/ows_logo.png') }}" class="h-10 mt-20 sm:h-40" />
+            <a href="{{ route('student_showlogin') }}">
+                <button type="button"
+                    class="mt-20 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                    GET STARTED
+                </button>
+            </a>
+        </div>
+    </div>
 </div>
+
+
 
 @include('partials.__footer')
