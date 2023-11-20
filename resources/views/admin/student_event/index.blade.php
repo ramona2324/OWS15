51d8a9e760bb6d27aa0313f9291dc82d990037d7
raffdevstack <raffy77sm@gmail.com>
2023-11-21 03:34:37 +0800

    done some draft events page

--- resources/views/admin/student_event/index.blade.php
+++ resources/views/admin/student_event/index.blade.php
@@ -18,57 +18,24 @@
                 <ol class="inline-flex items space-x-1">
                     <li class="inline-flex items-center">
                         <a href="{{ route('admin_dashboard') }}"
-                            class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-blue-600 ">
-                            <span class="px-1 material-symbols-rounded" style="font-size:20px">dashboard</span>
-                            Dashboard
+                            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 ">
+                            <span class="px-1 material-symbols-rounded" style="font-size:20px">how_to_reg</span>
+                            Student Events
                         </a>
                     </li>
 
-                    <li aria-current="page">
-                        <div class="flex items-center">
-                            <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 9 4-4-4-4" />
-                            </svg>
-                            <p class=" text-sm font-medium text-gray-700 hover:text-blue-600 ">
-                                Admins
-                            </p>
-                        </div>
-                    </li>
                 </ol>
             </nav>
-            <div class="flex">
-                <!-- Previous Button -->
-                <a href=" {{ route('admin_dashboard') }}"
-                    class="flex items-center justify-center px-3 h-8 me-3 text-sm font-medium text-gray-500 bg-white  rounded-lg hover:bg-gray-100 hover:text-gray-700 ">
-                    <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M13 5H1m0 0 4 4M1 5l4-4" />
-                    </svg>
-                    back
-                </a>
-                {{-- next button code --}}
-                {{-- <a href="#"
-                    class="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 ">
-                    
-                    <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a> --}}
-            </div>
+
         </div>
 
         {{-- title + button container --}}
         <div class="flex items-center justify-center py-2 mb-4 rounded text-slate-800 ">
             <h2 class=" text-lg font-bold leading-none tracking-tight text-slate-800 md:text-xl ">
-                Manage Admins
+                Student Events
             </h2>
             {{-- for medium screens and up --}}
-            <a href=" {{ route('admin_create') }} " class="block absolute right-8 md:right-10">
+            <a href="  " class="block absolute right-8 md:right-10">
                 <button type="submit"
                     class="hidden md:inline-flex items-center px-1 py-1 text-sm font-medium text-center bg-gray-200 rounded-lg hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                     <div
@@ -77,7 +44,7 @@
                             add
                         </span>
                     </div>
-                    <h3 class="px-2">Add Admin</h3>
+                    <h3 class="px-2">Add Event</h3>
                 </button>
                 {{-- for small screens --}}
                 <button type="submit"
@@ -92,43 +59,22 @@
             </a>
         </div>
 
-        <div class=" flex flex-col lg:flex-row lg:flex-wrap items-center mb-4 rounded ">
-            @foreach ($admins as $admin)
-                @php $default_profile = "https://api.dicebear.com/7.x/initials/svg?seed=".$admin->admin_fname."" @endphp
-                <a href=" {{ route('admin_profile', ['admin' => $admin->admin_id]) }} " 
-                    class="border-box lg:w-80 truncate w-full m-2 p-2 bg-white border border-gray-300 rounded-lg hover:shadow-lg shadow-sm ">
-                    <div class="flex items-center p-2">
-                        <img class="mr-2 border-4 h-10 w-10 rounded-full"
-                            src="{{ $admin->admin_image ? asset('/storage/admin/thumbnail/' . 'small_' . $admin->admin_image) : "$default_profile" }}">
-                        <div>
-                            <h5 class=" text-lg font-semibold tracking-tight text-gray-700 ">
-                                {{ $admin->admin_fname }} {{ $admin->admin_lname }}
-                            </h5>
-                            <p class="text-ellipsis font-normal text-sm text-gray-500 ">
-                                {{ $admin->email }}
-                            </p>
-                        </div>
-                    </div>
-                    <div class="p-1 px-2 pt-0 mt-2 flex flex-row justify-between text-gray-500">
-                        <div class="flex items-center">
-                            <span class="text-sm material-symbols-rounded mr-1 text-md">
-                                admin_panel_settings
-                            </span>
-                            <p class=" font-normal text-sm  ">
-                                {{ $admin->adminType->admintype_name ?? 'N/A' }}
-                            </p>
-                        </div>
-                        <div class="flex items-center">
-                            <span class="text-sm material-symbols-rounded mr-1">
-                                meeting_room
-                            </span>
-                            <p class=" font-normal text-sm  ">
-                                {{ $admin->office->office_name ?? 'N/A' }}
-                            </p>
-                        </div>
-                    </div>
-                </a>
-            @endforeach
+        <div class=" flex bg-red-500 flex-col items-center mb-4 rounded ">
+
+            <a href=" "
+                class="flex justify-start border-box truncate w-full mb-2 p-2 bg-white border border-gray-300 rounded-lg hover:shadow-lg shadow-sm ">
+                <div class="bg-blue-500 w-48">
+                    <h4>days to go</h4>
+                </div>
+                <div class="bg-yellow-500 w-full">
+                    <h4>This is the Title</h4>
+                </div>
+                <button type="button"
+                    class=" focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 ">
+                    View Details
+                </button>
+            </a>
+
 
         </div>
     </div>
