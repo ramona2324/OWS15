8b5da6c8f32ed50dd446bf11ea6a5f1ee8529b55
Pedong14321 <alfredjrvitangcor@gmail.com>
2023-11-22 00:27:01 +0800

    Update index.blade.php

--- resources/views/admin/scholarship/index.blade.php
+++ resources/views/admin/scholarship/index.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Font1:style1,style2|Font2:style1,style2&display=swap">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.js">
     @include('partials.__header')
-    @include('partials.__sidebar')
+    @include('partials.__admin_sidebar')
     <title>Scholarships</title>
     <style>
         body {
@@ -120,7 +120,7 @@
         <p style="font-weight: bolder; font-size: x-large;">${scholarshipName.value}</p>
         <br>
         <div class="tablet-stack flex flex-col md:flex-row md:space-x-2 items-end justify-end w-full mt-2.5 gap-2">
-            <a href="{{ route('admin.scholarshipsgrantees') }}">
+            // <a href="{{''}}">
                 <button class="btn-del text-base sm:text-xl text-white px-4 py-1.5 focus:ring-4 focus:outline-none rounded-full w-full md:w-auto text-center md:text-left">
                     Grantees
                 </button>
@@ -306,7 +306,10 @@
                     <!-- <form action="  search" method="post"> -->
                     <!-- @csrf -->
                     <input type="text" id="search-dropdown" name="skey" class="rounded-lg block p-2.5 h-10 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search..." style="border-color: #630606">
-                    <!-- <a href="{{ route('admin.search') }}" > -->
+                    <!-- 
+                    <a href="{{('') }}" > 
+
+                 -->
                     <button type="submit" id="searchBtn" class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white rounded-r-lg border focus:ring-4 focus:outline-none dark:bg-blue-600" style="border-color: #630606; background-color: #630606">
                         <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -323,7 +326,7 @@
 
                                 // Send an AJAX request to your Laravel route
                                 $.ajax({
-                                    url: "{{ route('admin.search') }}", // Use the named route
+                                    url: "{{ ('') }}", // Use the named route
                                     method: 'GET',
                                     data: {
                                         searchTerm: searchTerm
