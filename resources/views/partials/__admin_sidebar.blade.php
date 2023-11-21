de066c93f06e219976d3a91d55d0deef4fe798a4
Pedong14321 <alfredjrvitangcor@gmail.com>
2023-11-22 00:27:35 +0800

    Update __admin_sidebar.blade.php

--- resources/views/partials/__admin_sidebar.blade.php
+++ resources/views/partials/__admin_sidebar.blade.php
@@ -50,7 +50,7 @@
                 </a>
             </li>
             <li>
-                <a href="{{ route('admin_scholarships') }}" class="@include('partials.__admin_sidenav_class_attrib')">
+                <a href="{{ route('admin_scholarship') }}" class="@include('partials.__admin_sidenav_class_attrib')">
                     <span class="material-symbols-rounded">
                         school
                     </span>
