af002f059b4758bd166a34d41d11eadeafd972ae
raffdevstack <raffy77sm@gmail.com>
2023-11-29 23:40:18 +0800

    added function for getting days left

--- app/Models/StudentEvent.php
+++ app/Models/StudentEvent.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class StudentEvent extends Model
 {
@@ -18,4 +19,20 @@ class StudentEvent extends Model
         'event_time_in',
         'event_time_out',
     ];
+
+    // Accessor for the dynamic attribute
+    public function getDaysLeftAttribute()
+    {
+        // Calculate the difference in days
+        $eventDate = Carbon::parse($this->attributes['event_date']);
+        $currentDate = Carbon::now();
+        
+        // If the event date is in the past, return 0
+        if ($currentDate->gt($eventDate)) {
+            return 0;
+        }
+    
+        $daysLeft = $eventDate->diffInDays($currentDate);
+        return $daysLeft;
+    }
 }
