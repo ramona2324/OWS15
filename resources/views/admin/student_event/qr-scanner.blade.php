af439596735700fa9a7d42a165aec897aa5497fe
raffdevstack <raffy77sm@gmail.com>
2023-12-07 22:59:40 +0800

    tried getting back cam first

--- resources/views/admin/student_event/qr-scanner.blade.php
+++ resources/views/admin/student_event/qr-scanner.blade.php
@@ -11,8 +11,7 @@
     <a href=" {{ route('admin_stud_events') }} ">
         <button type="button"
             class="absolute top-6 end-6 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center ">
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 14">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
             </svg>
@@ -54,6 +53,8 @@
     Instascan.Camera.getCameras()
         .then(function(cameras) {
             if (cameras.length > 0) {
+                scanner.start(cameras[1]);
+            } else if (cameras.length > 0) {
                 scanner.start(cameras[0]);
             } else {
                 alert('No cameras found');
