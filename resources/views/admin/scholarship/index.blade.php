b7cee95e1ff7e4f5115c20bfcff9a297f68fe2c2
raffdevstack <raffy77sm@gmail.com>
2023-12-07 11:36:34 +0800

    improved scholarship nav view

--- resources/views/admin/scholarship/index.blade.php
+++ resources/views/admin/scholarship/index.blade.php
@@ -36,18 +36,30 @@
         </div>
 
         {{-- main content --}}
-        <div class="mt-2 bg--300 flex justify-end">
+        <div class="mt-2 bg--300 gap-4 flex justify-end">
+            {{-- for lg --}}
             <span class="flex gap-1">
-                <span class="bg-gray-100 text-red-900 p-1 flex items-center rounded-full">
-                    <span class="material-symbols-rounded" style="font-size: 20px">search</span>
+                <span class="bg-gray-100 justify-center text-md p-1 flex items-center rounded-full hover:bg-yellow-200 transition-all">
+                    <div class="bg-white relative flex rounded-full">
+                        <input type="text" class="lg:flex hidden rounded-full px-2 min-h-full focus:outline-none">
+                        <span class="material-symbols-rounded p-1 bg-white rounded-full"
+                            style="font-size: 16px">search</span>
+                    </div>
+                    <a href="" class="flex items-center">
+                        <button class="px-2 lg:flex hidden rounded-full">Search</button>
+                    </a>
                 </span>
-                <span class="bg-gray-100 text-red-900 p-1 flex items-center rounded-full">
-                    <span class="material-symbols-rounded" style="font-size: 20px">add</span>
+                <span class="bg-gray-100 justify-center text-md p-1 flex items-center rounded-full hover:bg-yellow-200 transition-all">
+                    <a href="" class="flex items-center">
+                        <span class="material-symbols-rounded p-1 bg-white rounded-full"
+                            style="font-size: 16px">add</span>
+                        <span class="lg:flex hidden px-2 rounded-full">Add Scholarship</span>
+                    </a>
                 </span>
             </span>
         </div>
         <div class="bg--300 mt-2 mb-4 grid lg:grid-cols-3 md:grid-cols-2 gap-2 ">
-            <div class="bg--300 border rounded-lg p-2">
+            <div class="bg--300 border rounded-lg p-2 hover:shadow-md transition-all">
                 <div class="flex items-center gap-1">
                     <span class="material-symbols-rounded" style="font-size: 20px">school</span>
                     <h4 class="truncate">CHED Scholarship Program</h4>
