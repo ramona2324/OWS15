b3efe3e4aaaf321fe2cb1814efcb9a4f082eb9c0
raffdevstack <raffy77sm@gmail.com>
2023-12-19 16:36:52 +0800

    updated admin profile

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -2,31 +2,24 @@
 
 @include('partials.__admin_sidebar')
 
-<div class=" md:ml-64 pb-4">
-    <div class=" px-4 py-6 m-4 flex justify-end">
-        @include('partials.__admin_profile')
-    </div>
-    <div class="p-4 m-4 shadow-lg bg-white border-gray-200 rounded-lg dark:border-gray-700">
-        {{-- breadcrumbs container --}}
-        <div class="flex items-center py-2 mb-4 rounded  dark:bg-gray-800">
+{{-- right side section --}}
+<div class="md:ml-60">
+
+    {{-- reusable page header --}}
+    @include('partials.__admin_pageheader')
+
+    {{-- main content --}}
+    <div class="p-4 m-4 shadow-lg bg-white border-gray-200 rounded-lg " style="min-height: 90vh">
+        {{-- navigation container --}}
+        <div class="justify-between flex items-center  mb-4 rounded  ">
+            {{-- breadcrumb nav container --}}
             <nav class="flex" aria-label="Breadcrumb">
                 <ol class="inline-flex items space-x-1">
                     <li class="inline-flex items-center">
                         <a href="{{ route('admin_dashboard') }}"
-                            class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                            class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-blue-600 ">
                             <span class="px-1 material-symbols-rounded" style="font-size:20px">dashboard</span>
                             Dashboard
-                        </a>
-                    </li>
-                    <li class="inline-flex items-center">
-                        <a href="{{ route('admin_manage') }}"
-                            class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                            <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 9 4-4-4-4" />
-                            </svg>
-                            Admins
                         </a>
                     </li>
                     <li aria-current="page">
@@ -36,22 +29,48 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2" d="m1 9 4-4-4-4" />
                             </svg>
-                            <p
-                                class=" text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                            <a href="{{ route('admin_manage') }}">
+                                <p class=" text-sm font-medium text-gray-400 hover:text-blue-600 ">
+                                    Admins
+                                </p>
+                            </a>
+                        </div>
+                    </li>
+                    <li aria-current="page">
+                        <div class="flex items-center">
+                            <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 9 4-4-4-4" />
+                            </svg>
+                            <p class=" text-sm font-medium text-gray-700 hover:text-blue-600 ">
                                 Profile
                             </p>
                         </div>
                     </li>
                 </ol>
             </nav>
+            <div class="flex">
+                <!-- Previous Button -->
+                <a href=" {{ route('admin_manage') }}"
+                    class="flex items-center justify-center px-3 h-8 me-3 text-sm font-medium text-gray-500 bg-white  rounded-lg hover:bg-gray-100 hover:text-gray-700 ">
+                    <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M13 5H1m0 0 4 4M1 5l4-4" />
+                    </svg>
+                    back
+                </a>
+            </div>
         </div>
-         {{-- title + button container --}}
-         <div class="flex items-center justify-center py-2 mb-4 rounded text-slate-800 dark:bg-gray-800">
-            <h2 class=" text-lg font-bold leading-none tracking-tight text-slate-800 md:text-xl dark:text-white">
+
+        {{-- title + button container --}}
+        <div class="flex items-center justify-center py-2 mb-4 rounded text-slate-800 ">
+            <h2 class=" text-lg font-bold leading-none tracking-tight text-slate-800 md:text-xl ">
                 Admin Profile
             </h2>
-
         </div>
+
         <p><strong>Name:</strong> {{ $admin->admin_fname }} {{ $admin->admin_lname }}</p>
         <p><strong>Email:</strong> {{ $admin->email }}</p>
     </div>
